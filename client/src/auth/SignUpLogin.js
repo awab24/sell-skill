83668f68021bad52e3fcdbd2c6efb67ccc78e11f
Awab Hassan <awab1hassan1@gmail.com>
2024-07-16 23:57:37 +0300

    sdrw

--- client/src/auth/SignUpLogin.js
+++ client/src/auth/SignUpLogin.js
@@ -1,12 +1,11 @@
-import React, { useState } from 'react'
-import {Button, Alert, Breadcrumb, Card, Form, Container, Col,Row} from 'react-bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import axios from 'axios'
-import { GoogleLogin } from '@react-oauth/google'
-import {useNavigate} from 'react-router-dom'
-import { useDispatch } from 'react-redux'
-import { changeAuth, setProviderId, setProviderOrClientId } from '../reducers/reducers'
-
+import React, { useState } from 'react';
+import { Button, Alert, Card, Form, Container, Row, Col } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import { GoogleLogin } from '@react-oauth/google';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { changeAuth, setProviderId, setProviderOrClientId } from '../reducers/reducers';
 
 function SignUpLogin() {
   const dispatch = useDispatch();
@@ -18,81 +17,65 @@ function SignUpLogin() {
   let responseProviderId;
   let clientToken;
   let providerToken;
-  const [clientResult, setClientResult] = useState(true)
- const [providerResult, setProviderResult]= useState(true)
+  const [clientResult, setClientResult] = useState(true);
+  const [providerResult, setProviderResult] = useState(true);
 
-
-  const googleSuccess = async(response) => {
-    const {credential} = response;
+  const googleSuccess = async (response) => {
+    const { credential } = response;
     const payload = JSON.parse(atob(credential.split('.')[1]));
-    const {email} = payload;
-    console.log('email => '+email)
-  
-    try {
-      responseClient = await axios.post('https://sell-skill-d7865032728d.herokuapp.com/api/endpoints/clientSignIn', {email:email, password: ''});
-      clientToken  = responseClient.data;
-      
-      localStorage.setItem('clientToken', JSON.stringify(clientToken));
-        responseClient && navigate('/Client');
+    const { email } = payload;
+    console.log('email => ' + email);
 
+    try {
+      responseClient = await axios.post('https://sell-skill-d7865032728d.herokuapp.com/api/endpoints/clientSignIn', { email: email, password: '' });
+      clientToken = responseClient.data;
+
+      localStorage.setItem('clientToken', JSON.stringify(clientToken));
+      responseClient && navigate('/Client');
     } catch (error) {
-      setClientResult(false)
+      setClientResult(false);
       try {
-        responseProvider = await axios.post('https://sell-skill-d7865032728d.herokuapp.com/api/endpoints/providerSignIn', {email:email, password: ''});
-        providerToken  = responseProvider.data;
+        responseProvider = await axios.post('https://sell-skill-d7865032728d.herokuapp.com/api/endpoints/providerSignIn', { email: email, password: '' });
+        providerToken = responseProvider.data;
         localStorage.setItem('providerToken', JSON.stringify(providerToken));
-          responseProvider && navigate('/provider');
-    
+        responseProvider && navigate('/provider');
       } catch (error) {
-        setProviderResult(false)
+        setProviderResult(false);
       }
     }
-
-
-  }
+  };
 
   const handleClick = async () => {
     try {
-
-
       try {
         responseClient = await axios.post('https://sell-skill-d7865032728d.herokuapp.com/api/endpoints/clientSignIn', signInData);
-        clientToken  = responseClient.data;
-        console.log('token ======================================================> '+clientToken)
+        clientToken = responseClient.data;
+        console.log('token ======================================================> ' + clientToken);
         localStorage.setItem('clientToken', JSON.stringify(clientToken));
-
       } catch (error) {
-        setClientResult(false)
+        setClientResult(false);
         try {
           responseProvider = await axios.post('https://sell-skill-d7865032728d.herokuapp.com/api/endpoints/providerSignIn', signInData);
-          providerToken  = responseProvider.data;
-          console.log('token ======================================================> '+providerToken)
+          providerToken = responseProvider.data;
+          console.log('token ======================================================> ' + providerToken);
           localStorage.setItem('providerToken', JSON.stringify(providerToken));
-    
-
         } catch (secondError) {
-          setProviderResult(false)
+          setProviderResult(false);
           console.error('Both sign-in attempts failed:', secondError);
-          return; // or handle the error appropriately
+          return;
         }
       }
-        
-
-      // Store the token securely in localStorage
-
-  
 
       localStorage.setItem('authenticated', 'true');
       dispatch(changeAuth(true));
-      
-      responseClient && navigate('/Client');
-      console.log('responseClient ====================================>>>>>>>>>> '+responseClient)
-      responseProvider && navigate('/provider')
-      if(responseClient || responseProvider){
-        const responseProviderOrClientId = await axios.get('https://sell-skill-d7865032728d.herokuapp.com/api/endpoints/providerOrClientId')
-        console.log(dispatch(setProviderOrClientId(responseProviderOrClientId.data)))
-      }
 
+      responseClient && navigate('/Client');
+      console.log('responseClient ====================================>>>>>>>>>> ' + responseClient);
+      responseProvider && navigate('/provider');
+      if (responseClient || responseProvider) {
+        const responseProviderOrClientId = await axios.get('https://sell-skill-d7865032728d.herokuapp.com/api/endpoints/providerOrClientId');
+        console.log(dispatch(setProviderOrClientId(responseProviderOrClientId.data)));
+      }
     } catch (error) {
       console.error('Sign-in failed:', error);
       setNotAllowed(true);
@@ -101,57 +84,48 @@ function SignUpLogin() {
     }
   };
 
-
- 
-  
-
-
   return (
-    <div  style={{'height':'630px','backgroundColor': 'blue'}}>
-    <Container>
-    <Card style={{'position': 'relative', 'top': '100px', 'height': '500px', 'backgroundColor': 'black', 'color': 'blue', 'borderRadius': '40px'}}>
-      <Card style={{'position':'relative','top':'50px','left':'270px','backgroundColor': 'blue', 'width': '600px', 'height': '330px','borderRadius':'40px'}}>
-      <Card.Title style={{'position': 'absolute', 'left': '270px', 'display':'flex', 'top': '50px', 'fontSize': '30px', }}>
-        <b style={{'color': 'black'}}>
-        Login
-        </b>
-
-         </Card.Title>
-         <Form style={{'position': 'relative', 'top': '130px','left':'30px', 'borderRadius': '90px', 'width':'500px'}} >
-             <Form.Control placeholder='email or username' onChange={(e)=> setSignInData({...signInData, email: e.target.value})}/>
-        </Form>
-        <Form style={{'position': 'relative', 'top': '160px', 'left':'30px', 'width':'500px'}} >
-          <Form.Control placeholder='password' onChange={(e)=> setSignInData({...signInData, password: e.target.value})}/>
-        </Form>
-
-        <Button onClick={handleClick} style={{'position': 'relative', 'top': '190px' }}><b style={{'color': 'black', 'fontSize': '16px'}}>Log in</b></Button>
-
-        <a href="/sign-up-client-provider " style={{'backgroundColor': 'black', 'position': 'relative', 'top':'230px', 'left': '160px'}}>
-        don't have an account?!  let's sign up
-      </a>
-      <div style={{'position': 'relative', 'top':'250px', 'left': '170px'}}>
-      <GoogleLogin onSuccess={googleSuccess}/>
-      </div>
-     
-
-     {
-      notAllowed && <div style={{'backgroundColor': 'blue', 'position': 'relative', }}><b>not allowed please make sure you have an account</b></div>
-     }
-      </Card>
-    
-    
-
-    {
-      !clientResult && !providerResult && <div>email or password might be incorrect</div>
-    }
-
-    
-    </Card>
-    </Container>
-    
+    <div style={{ height: '100vh', backgroundColor: '#007bff' }}>
+      <Container className="d-flex justify-content-center align-items-center" style={{ height: '100%' }}>
+        <Card style={{ width: '100%', maxWidth: '500px', backgroundColor: '#000', color: '#007bff', borderRadius: '20px' }}>
+          <Card.Body>
+            <Card.Title className="text-center" style={{ color: '#fff', fontSize: '24px' }}>
+              <b>Login</b>
+            </Card.Title>
+            <Form>
+              <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Control type="email" placeholder="Email or username" onChange={(e) => setSignInData({ ...signInData, email: e.target.value })} />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Control type="password" placeholder="Password" onChange={(e) => setSignInData({ ...signInData, password: e.target.value })} />
+              </Form.Group>
+              <Button variant="primary" type="button" onClick={handleClick} className="w-100">
+                Log in
+              </Button>
+            </Form>
+            <div className="text-center mt-3">
+              <a href="/sign-up-client-provider" style={{ color: '#007bff' }}>
+                Don't have an account? Let's sign up
+              </a>
+            </div>
+            <div className="text-center mt-3">
+              <GoogleLogin onSuccess={googleSuccess} />
+            </div>
+            {notAllowed && (
+              <Alert variant="danger" className="mt-3">
+                Not allowed, please make sure you have an account
+              </Alert>
+            )}
+            {!clientResult && !providerResult && (
+              <Alert variant="danger" className="mt-3">
+                Email or password might be incorrect
+              </Alert>
+            )}
+          </Card.Body>
+        </Card>
+      </Container>
     </div>
-
-  )
+  );
 }
 
-export default SignUpLogin
+export default SignUpLogin;
