42c2e99f6ee1029f26fa0c45492c204897aa37f4
Awab Hassan <awab1hassan1@gmail.com>
2024-07-17 13:56:15 +0300

    eqWE

--- server/controllers/controllers.js
+++ server/controllers/controllers.js
@@ -733,7 +733,7 @@ await ProviderModel.findByIdAndUpdate(
 
 export const getMessagesFromClientIntoProvider = async(req, res) => {
     const provider = await ProviderModel.findById(providerOrClientId)
-    console.log('messages from back =========>  ',messages)
+
     res.send(provider.messages)
 }
 
