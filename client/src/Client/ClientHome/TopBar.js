9d37f9a046fa67f5ab5d29bb7e792edffc41b034
Awab Hassan <awab1hassan1@gmail.com>
2024-07-19 16:32:00 +0300

    RTg

--- client/src/Client/ClientHome/TopBar.js
+++ client/src/Client/ClientHome/TopBar.js
@@ -1,90 +1,135 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Button, Card, Alert } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import { Buffer } from 'buffer';
 import styled from 'styled-components';
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
 
-function ImgSlider() {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-  };
+function TopBar() {
+  const [checkNewMessages, setCheckNewMessages] = useState(false);
+  const [newProposals, setNewProposals] = useState(false);
+  const [profilePictureSrc, setProfilePictureSrc] = useState('');
+  const [name, setName] = useState('');
+
+  useEffect(() => {
+    const fetchCheckClientNewMessage = async () => {
+      const response = await axios.get('https://sell-skill.com/api/endpoints/checkClientNewMessage');
+      setCheckNewMessages(response.data);
+    };
+    fetchCheckClientNewMessage();
+  }, []);
+
+  useEffect(() => {
+    const checkNewProposal = async () => {
+      const response = await axios.get('https://sell-skill.com/api/endpoints/checkClientNewProposals');
+      setNewProposals(response.data);
+    };
+    checkNewProposal();
+  }, []);
+
+  useEffect(() => {
+    fetch('https://sell-skill.com/api/endpoints/getClientProfileData')
+      .then(response => response.json())
+      .then(result => setName(result.name));
+  }, []);
+
+  useEffect(() => {
+    const fetchImage = async () => {
+      try {
+        const response = await axios.get('https://sell-skill.com/api/endpoints/getClientProfilePicture', {
+          responseType: 'arraybuffer'
+        });
+
+        const base64 = Buffer.from(response.data, 'binary').toString('base64');
+        const mimeType = response.headers['content-type'];
+        setProfilePictureSrc(`data:${mimeType};base64,${base64}`);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    fetchImage();
+  }, [name]);
 
   return (
-    <ImgSliderContainer>
-      <BlueBorder />
-      <Carousel {...settings}>
-        <Wrap>
-          <img src="/images/Slider1.png" alt="" />
-        </Wrap>
-        <Wrap>
-          <img src="/images/Slider5.jpg" alt="" />
-        </Wrap>
-        <Wrap>
-          <img src="/images/Slider7.jpg" alt="" />
-        </Wrap>
-        <Wrap>
-          <img src="/images/Slider8.png" alt="" />
-        </Wrap>
-        <Wrap>
-          <img src="/images/Slider9.webp" alt="" />
-        </Wrap>
-      </Carousel>
-    </ImgSliderContainer>
+    <TopBarContainer>
+      {checkNewMessages && <Alert variant="danger">You have new messages!</Alert>}
+      {newProposals && <Alert variant="danger">You have got proposal!</Alert>}
+
+      <NavLinks>
+        <NavLink href="/report">Make Report</NavLink>
+        <NavLink href="/notifications">Notifications & Providers</NavLink>
+        <NavLink href="/client-received-messages">Messages</NavLink>
+        <NavLink href="/client-posts">My Posts</NavLink>
+      </NavLinks>
+
+      <ProfileCard>
+        <Card.Title>{name}</Card.Title>
+        {
+          profilePictureSrc && 
+          <ProfileImage src={profilePictureSrc || "/images/NormalProfile.jpg"} />
+        }
+
+      </ProfileCard>
+    </TopBarContainer>
   );
 }
 
-export default ImgSlider;
+export default TopBar;
 
-const ImgSliderContainer = styled.div`
-  width: 100%;
-  max-width: 1200px;
-  margin: 0 auto;
+// Styled Components
+const TopBarContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: blue;
   padding: 20px;
-  box-sizing: border-box;
+  color: white;
+
+  @media (max-width: 768px) {
+    padding: 10px;
+  }
 `;
 
-const BlueBorder = styled.div`
-  height: 5px;
-  background-color: #007bff;
+const NavLinks = styled.div`
+  display: flex;
+  justify-content: space-around;
   margin-bottom: 20px;
-`;
 
-const Carousel = styled(Slider)`
-  .slick-prev:before,
-  .slick-next:before {
-    color: #000;
-  }
-  ul li button {
-    &:before {
-      font-size: 10px;
-      color: white;
-    }
-  }
-  li.slick-active button:before {
-    color: white;
-  }
-  .slick-list {
-    overflow: hidden;
-  }
-  .slick-prev {
-    left: 10px;
-    z-index: 1;
-  }
-  .slick-next {
-    right: 10px;
-    z-index: 1;
+  @media (max-width: 768px) {
+    flex-direction: column;
+    align-items: center;
   }
 `;
 
-const Wrap = styled.div`
-  img {
-    width: 100%;
-    border-radius: 10px;
-    box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
+const NavLink = styled.a`
+  color: white;
+  text-decoration: none;
+  font-weight: bold;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`;
+
+const ProfileCard = styled(Card)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 200px;
+  margin-left: auto;
+
+  @media (max-width: 768px) {
+    width: 150px;
+  }
+`;
+
+const ProfileImage = styled.img`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  margin-left: 10px;
+
+  @media (max-width: 768px) {
+    width: 40px;
+    height: 40px;
   }
 `;
