08c564dc5a4f38032b4d365e4e383f42c4ae2359
Awab Hassan <awab1hassan1@gmail.com>
2024-07-19 16:21:14 +0300

    AWEfweq

--- client/src/Client/ClientHome/MainHome.js
+++ client/src/Client/ClientHome/MainHome.js
@@ -7,6 +7,7 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { Card, Button as BootstrapButton } from 'react-bootstrap';
 import styled from 'styled-components';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function MainHome() {
   const [permission, setPermission] = useState(false);
@@ -56,18 +57,19 @@ function MainHome() {
 export default MainHome;
 
 const MainContainer = styled.div`
-  background-color: blue;
+  background-color: #f0f2f5;
   min-height: 100vh;
   padding-bottom: 360px;
   display: flex;
   flex-direction: column;
   align-items: center;
+  color: #333;
 `;
 
 const SignUpContainer = styled.div`
   position: relative;
   top: 170px;
-  background-color: black;
+  background-color: #fff;
   height: 200px;
   width: 80%;
   max-width: 800px;
@@ -75,11 +77,19 @@ const SignUpContainer = styled.div`
   justify-content: center;
   align-items: center;
   border-radius: 10px;
-  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
 
   .sign-up-card {
     width: 100%;
     max-width: 400px;
     text-align: center;
+  }
+
+  button {
+    background-color: #007bff;
+    border: none;
+    &:hover {
+      background-color: #0056b3;
+    }
   }
 `;
