cb20a0b54e455f05aee210f76c3cc63913e8f189
Awab Hassan <awab1hassan1@gmail.com>
2024-07-21 11:49:56 +0300

    ASfaA

--- client/src/Client/InvitationAcceptance/InvitationAcceptance.js
+++ client/src/Client/InvitationAcceptance/InvitationAcceptance.js
@@ -11,53 +11,53 @@ function InvitationAcceptance() {
     const [invitationAcceptances, setInvitationAcceptances] = useState([])
 
     useEffect(() => {
-      const killNewInvitationAcceptanceNotification = async() => {
-        await axios.patch('https://sell-skill.com/api/endpoints/killNewInvitationAcceptanceNotification')
-      }
-      killNewInvitationAcceptanceNotification();
+        const killNewInvitationAcceptanceNotification = async() => {
+            await axios.patch('https://sell-skill.com/api/endpoints/killNewInvitationAcceptanceNotification')
+        }
+        killNewInvitationAcceptanceNotification();
     }, [])
+
     useEffect(() => {
         const fetchInvitationsAcceptances = async() => {
             const result = await axios.get('https://sell-skill.com/api/endpoints/getInvitationAcceptance')
             setInvitationAcceptances(result.data)
             console.log('result data ====================================================> ', result.data, '  <===================================================================result data')
-          invitationAcceptances.map((InvitationAcceptance) => 
-          console.log('invitationAcceptance.invitationAcceptance  ==============================================================>  ',InvitationAcceptance.InvitationAcceptance,'  <<====================================================invitationAccepatance.invitationAcceptance'))
+            result.data.map((invitationAcceptance) => 
+                console.log('invitationAcceptance.invitationAcceptance  ==============================================================>  ', invitationAcceptance.invitationAcceptance, '  <<====================================================invitationAcceptance.invitationAcceptance'))
         };
         fetchInvitationsAcceptances()
     }, [])
 
     const handleGoToPayment = async(providerId) => {
-
         dispatch(setProposalId(providerId))
         navigate('/paypal')
     }
+
     const handleGoToMessage2 = async(providerId) => {
         dispatch(setProviderId(providerId))
         navigate('/client-access-messages')
     }
-  return (
-    <div>
-        {
-            invitationAcceptances.length > 0 ?
-            invitationAcceptances.map((InvitationAcceptance) => 
-            (
-            <>
-            <Card>{InvitationAcceptance.providerName+' '}has accepted your inivtation</Card>
-            <Button onClick={() => handleGoToPayment(InvitationAcceptance.providerId)}>
-        go to payment
-      </Button>
-      <Button onClick={() => handleGoToMessage2(InvitationAcceptance.providerId)}>
-        Message
-      </Button>
-            </>)
 
-            ): (<div>No invitation Acceptance</div>)
-        }
-      invitation acceptance
-
-    </div>
-  )
+    return (
+        <div>
+            {
+                invitationAcceptances.length > 0 ?
+                invitationAcceptances.map(({ invitationAcceptance }) => (
+                    <div key={invitationAcceptance.providerId}>
+                        <Card>{invitationAcceptance.providerName + ' '} has accepted your invitation</Card>
+                        <Button onClick={() => handleGoToPayment(invitationAcceptance.providerId)}>
+                            Go to payment
+                        </Button>
+                        <Button onClick={() => handleGoToMessage2(invitationAcceptance.providerId)}>
+                            Message
+                        </Button>
+                    </div>
+                )) : (
+                    <div>No invitation acceptance</div>
+                )
+            }
+        </div>
+    )
 }
 
 export default InvitationAcceptance
